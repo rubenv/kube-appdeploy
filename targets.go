29cf4a4083303037169a8559e5498277c6016fb1
Ruben Vermeersch <ruben@rocketeer.be>
2017-07-05 14:34:08 +0200

    Update import

--- targets.go
+++ targets.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 	"time"
 
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/rest"
 
 	"github.com/eapache/go-resiliency/retrier"
